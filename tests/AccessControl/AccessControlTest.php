c6c0c9f4db58545dd9c11e6b570d182c62b8b1dd
Tim Roediger <superdweebie@gmail.com>
2012-06-23 21:34:52 +1000

    Removed php 5.3 from travis. Added blank tests

---
+++ tests/AccessControl/AccessControlTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SdsDoctrineExtensionsTest\AccessControl;
+
+use SdsDoctrineExtensionsTest\BaseTest;
+
+class AccessControlTest extends BaseTest {
+
+    public function setUp(){
+
+
+    }
+
+    public function testBasicFunction(){
+
+        $this->markTestIncomplete();
+
+    }
+}
---
+++ tests/SdsDoctrineExtensionsTest/User/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SdsDoctrineExtensionsTest\User;
+
+use SdsDoctrineExtensionsTest\BaseTest;
+
+class UserTest extends BaseTest {
+
+    public function setUp(){
+
+
+    }
+
+    public function testBasicFunction(){
+
+        $this->markTestIncomplete();
+
+    }
+}
